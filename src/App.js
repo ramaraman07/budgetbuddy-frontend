9e7648be1bb74440c486766f464708afc6a427d3
Rama Raman Sarangi <rajasarangi6776@gmail.com>
2025-06-28 23:03:41 +0530

    Add global styles for all pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import AddExpense from "./pages/AddExpense";
+import "./App.css";
 
 function App() {
   return (
